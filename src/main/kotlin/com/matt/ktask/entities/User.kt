cd527f84696663a65d009162bae9f4dbde89e841
MateusVasc <mvaraujo2004@gmail.com>
2025-06-03 19:10:18 -0300

    feat: adds user entity

---
+++ src/main/kotlin/com/matt/ktask/entities/User.kt
@@ -0,0 +1,28 @@
+package com.matt.ktask.entities
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import jakarta.validation.constraints.Email
+import java.util.UUID
+
+@Entity
+@Table(name = "users")
+class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    var id: UUID? = null
+
+    @Column(nullable = false, unique = true)
+    var nickname: String = ""
+
+    @Email
+    @Column(nullable = false, unique = true)
+    var email: String = ""
+
+    @Column(nullable = false)
+    var password: String = ""
+}
