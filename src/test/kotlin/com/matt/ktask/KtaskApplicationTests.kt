078675366a981bda780ab0f124d888d4ac0e2b37
MateusVasc <mvaraujo2004@gmail.com>
2025-06-03 00:06:11 -0300

    feat: adds project initial structure

---
+++ src/test/kotlin/com/matt/ktask/KtaskApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.matt.ktask
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KtaskApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
