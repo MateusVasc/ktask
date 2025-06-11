532dd4ad0f6b92a9ee7503da132563e5c18df0d3
MateusVasc <mvaraujo2004@gmail.com>
2025-06-11 08:31:01 -0300

    feat: adds project entity and it's relationships

--- src/main/kotlin/com/matt/ktask/entities/Project.kt
+++ src/main/kotlin/com/matt/ktask/entities/Project.kt
@@ -1,16 +1,42 @@
 package com.matt.ktask.entities
 
+import jakarta.persistence.CascadeType
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.JoinTable
+import jakarta.persistence.ManyToMany
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.OneToMany
 import jakarta.persistence.Table
+import java.time.LocalDateTime
 import java.util.UUID
 
-@Entity
 @Table(name = "projects")
+@Entity
 class Project(
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    var id: UUID? = null
-)
+    var id: UUID? = null,
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "owner_id", nullable = false)
+    var owner: User
+) {
+    @Column(nullable = false, updatable = false)
+    var createdAt: LocalDateTime = LocalDateTime.now()
+
+    @ManyToMany
+    @JoinTable(
+        name = "project_participants",
+        joinColumns = [JoinColumn(name = "project_id")],
+        inverseJoinColumns = [JoinColumn(name = "user_id")]
+    )
+    var participants: MutableList<User> = mutableListOf()
+
+    @OneToMany(mappedBy = "project", cascade = [CascadeType.ALL], orphanRemoval = true)
+    var boards: MutableList<Board> = mutableListOf()
+}
